447d89a340fc153666347739e54c1553828d0071
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-04 01:55:39 +0530

    feat : added disabled button

--- components/button/src/js/properties.js
+++ components/button/src/js/properties.js
@@ -27,10 +27,10 @@ const elements = [
     elementCodeClassPath:
       ".multiple-button .import-html .code-tabs--container .code",
     elementCodeInnerText: `<iframe
-		src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Csection%2520class%253D%2522avatar-badge-container%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520xlg-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520xlg-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520lg-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520lg-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520md-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520md-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520sm-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520sm-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%253C%252Fsection%253E"
-		style="width: 850px; height: 670px; border:0; transform: scale(1); overflow:hidden;"
-		sandbox="allow-scripts allow-same-origin">
-	  </iframe>`,
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522button-container%2522%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520primary-card-btn%2522%253E%250A%2520%2520%2520%2520%253Cspan%2520class%253D%2522primary-cart-icon%2522%253E%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%253Cp%2520class%253D%2522primary-cart-text%2522%253EAdd%2520to%2520Cart%253C%252Fp%253E%250A%2520%2520%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522primary-cart-icon%2522%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 816px; height: 428px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
     elementJSCodeClassPath:
       ".multiple-button .import-html .code-tabs--container .code-js",
     elementJSCodeInnerText: "NA",
@@ -50,15 +50,58 @@ const elements = [
     elementCodeClassPath:
       ".toggle-button .import-html .code-tabs--container .code",
     elementCodeInnerText: `<iframe
-		src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Csection%2520class%253D%2522avatar-badge-container%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520xlg-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520xlg-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520lg-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520lg-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520md-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520md-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522avatar-badge-divison%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Favatar.jpg%2522%2520alt%253D%2522avatar1%2522%2520class%253D%2522avatar%2520sm-img%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cdiv%2520class%253D%2522avatar-badge-status%2520sm-badge%2522%253E%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%253C%252Fsection%253E"
-		style="width: 850px; height: 670px; border:0; transform: scale(1); overflow:hidden;"
-		sandbox="allow-scripts allow-same-origin">
-	  </iframe>`,
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Csection%2520class%253D%2522toggle-button-container%2522%253E%250A%2520%2520%2520%2520%253Clabel%2520class%253D%2522switch%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cinput%2520type%253D%2522checkbox%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520class%253D%2522slider%2522%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%253C%252Flabel%253E%250A%253C%252Fsection%253E"
+    style="width: 496px; height: 298px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
     elementJSCodeClassPath:
       ".toggle-button .import-html .code-tabs--container .code-js",
     elementJSCodeInnerText: "NA",
-  }
-
+  },
+  {
+    name: "Floating Button",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .floating-button .import-html",
+    elementCodeFilePath: "./src/html/floating-button.html",
+    elementCodeLocation:
+      ".floating-button .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".floating-button .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".floating-button .import-html .code-tabs--content",
+    elementCodeClassPath:
+      ".floating-button .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522floating-button-container%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522floating-button%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-plus%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 478px; height: 279px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".floating-button .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  },
+  {
+    name: "Disabled Button",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .disabled-button .import-html",
+    elementCodeFilePath: "./src/html/disabled-button.html",
+    elementCodeLocation:
+      ".disabled-button .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".disabled-button .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".disabled-button .import-html .code-tabs--content",
+    elementCodeClassPath:
+      ".disabled-button .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522disabled-button-container%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 757px; height: 261px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".disabled-button .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  },
 ];
 
 export { elements };
