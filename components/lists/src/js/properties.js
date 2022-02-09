23d9db4669b7bcaa8bb0b5d627e014ca57f9cd2c
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-10 01:06:01 +0530

    feat : added lists

---
+++ components/lists/src/js/properties.js
@@ -0,0 +1,61 @@
+/**
+ * rootCodeLocation: Where we need to import code for code tabs.
+ * elementCodeFilePath: The File Path for the component html file we need to import.
+ * elementCodeLocation: The location where we need to import the code for the component.
+ * handleCodeTabBtnClass: The classPath for the code tabs to call the handleCodeTab function which allows us to use the tabs.
+ * handleCodeTabCodeContentClass: Here we add content classpath
+ * elementCodeClassPath: This is the location where we store the HTML code.
+ * elementCodeInnerText: Here we add HTML code
+ * elementJSCodeClassPath: Here we add the classpath for JS code.
+ * elementJSCodeInnerText: Here we add the JS code.
+ * sandboxURLClass: Here we add the classPath for sandboxURL.
+ * sandboxURL: Here we add  the sandbox URL
+ */
+
+const elements = [
+  {
+    name: "Stacked List",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .stacked-list .import-html",
+    elementCodeFilePath: "./src/html/stacked-list.html",
+    elementCodeLocation:
+      ".stacked-list .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".stacked-list .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".stacked-list .import-html .code-tabs--content",
+    elementCodeClassPath:
+      ".stacked-list .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=htmlmixed&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cul%2520class%253D%2522stacked-list%2522%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522basic-card%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Finter-jersey.png%2522%2520alt%253D%2522%2522%2520class%253D%2522notification-image%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Notification%25201%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fh2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522basic-card%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Finter-jersey.png%2522%2520alt%253D%2522%2522%2520class%253D%2522notification-image%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Notification%25202%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fh2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Carticle%2520class%253D%2522basic-card%2520%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Finter-jersey.png%2522%2520alt%253D%2522%2522%2520class%253D%2522notification-image%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Ch2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Notification%25203%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fh2%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Farticle%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%253C%252Ful%253E"
+    style="width: 859px; height: 654px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".stacked-list .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  },
+  {
+    name: "Spaced List",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .spaced-list .import-html",
+    elementCodeFilePath: "./src/html/spaced-list.html",
+    elementCodeLocation:
+      ".spaced-list .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".spaced-list .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".spaced-list .import-html .code-tabs--content",
+    elementCodeClassPath: ".spaced-list .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=htmlmixed&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cul%2520class%253D%2522spaced-list%2522%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ca%2520href%253D%2522%2522%2520class%253D%2522no-link%2522%253EHome%253C%252Fa%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ca%2520href%253D%2522%2522%2520class%253D%2522no-link%2522%253EProducts%253C%252Fa%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%2520%2520%2520%2520%253Cli%2520class%253D%2522no-list%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ca%2520href%253D%2522%2522%2520class%253D%2522no-link%2522%253EProfile%253C%252Fa%253E%250A%2520%2520%2520%2520%253C%252Fli%253E%250A%253C%252Ful%253E"
+    style="width: 530px; height: 375px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".spaced-list .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  }
+];
+
+export { elements };
