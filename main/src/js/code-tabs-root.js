43fb8998a4dca1ffecb05ca808fed9af0227fb5a
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-11 21:12:25 +0530

    ref : changed js file to handle preview html and js

--- main/src/js/code-tabs-root.js
+++ main/src/js/code-tabs-root.js
@@ -1,32 +1,27 @@
-// Code Tabs  Root FUnction JS
-const handleCodeTab = (rdc_code_tabs_btn_path, rdc_code_tabs_content_path) => {
-  const rdc_code_tabs_btn = document.querySelectorAll(rdc_code_tabs_btn_path);
-  const rdc_code_tabs_content = document.querySelectorAll(
-    rdc_code_tabs_content_path
-  );
+const handleCodeTab = (codeTabsBtnPath, codeTabsContentpath) => {
+  const codeTabsBtn = document.querySelectorAll(codeTabsBtnPath);
+  const codeTabContent = document.querySelectorAll(codeTabsContentpath);
   const clearAlltabs = () => {
-    rdc_code_tabs_btn.forEach((item) => {
+    codeTabsBtn.forEach((item) => {
       if (item.classList.contains("code-tabs--btn-active")) {
         item.classList.remove("code-tabs--btn-active");
       }
     });
-    rdc_code_tabs_content.forEach((item) => {
+    codeTabContent.forEach((item) => {
       item.style.display = "none";
     });
   };
   const setActiveTab = (itemIndex) => {
     clearAlltabs();
-    console.log(rdc_code_tabs_btn, rdc_code_tabs_btn_path);
-    rdc_code_tabs_btn[itemIndex].classList.add("code-tabs--btn-active");
-    rdc_code_tabs_content[itemIndex].style.display = "block";
+    codeTabsBtn[itemIndex].classList.add("code-tabs--btn-active");
+    codeTabContent[itemIndex].style.display = "block";
   };
 
-  for (let i = 0; i < rdc_code_tabs_btn.length; i++) {
-    rdc_code_tabs_btn[i].addEventListener("click", () => {
+  for (let i = 0; i < codeTabsBtn.length; i++) {
+    codeTabsBtn[i].addEventListener("click", () => {
       setActiveTab(i);
     });
   }
-  // By default the first tab will be active
   setActiveTab(0);
 };
 
