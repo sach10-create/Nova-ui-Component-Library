537fb96b8bf677e4fee7da4aba68c90594c82134
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-20 00:20:23 +0530

    feat: added grids

---
+++ components/grid/src/js/index.js
@@ -0,0 +1,13 @@
+import { codeImport } from "../../../../main/src/js/import-html.js";
+import { elements } from "./properties.js";
+
+const callImportFunctions = async () => {
+  for (let i = 0; i < elements.length; i++) {
+    await codeImport(elements[i]);
+  }
+};
+
+const runScripts = async () => {
+  const importCode = await callImportFunctions();
+};
+runScripts();
---
+++ components/grid/src/js/properties.js
@@ -0,0 +1,49 @@
+
+const elements = [
+  {
+    name: "Grid 2",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .grid-2 .import-html",
+    elementCodeFilePath: "./src/html/grid-2.html",
+    elementCodeLocation:
+      ".grid-2 .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".grid-2 .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".grid-2 .import-html .code-tabs--content",
+    elementCodeClassPath:
+      ".grid-2 .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522jersey-container%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522jersey-container-img%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Finter-jersey.png%2522%2520%252F%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Csection%2520class%253D%2522jersey-content%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ch1%2520class%253D%2522jersey-title%2522%253EBarcelona%2520Home%2520Kit%253C%252Fh1%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522jersey-category%2522%253EJersey%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520class%253D%2522jersey-price-tag%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%253ERs.%25203999%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cstrike%253ERs.%25207999%253C%252Fstrike%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522discount%2522%253E%2850%2525%29%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fspan%253E%250A%2520%2520%2520%2520%253C%252Fsection%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522jersey-btn%2520jersey-cart%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520class%253D%2522cart-icon%2522%253E%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522cart-text%2522%253EAdd%2520to%2520Cart%253C%252Fp%253E%250A%2520%2520%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 782px; height: 521px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".grid-2 .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  },
+  {
+    name: "Grid 3",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .grid-3 .import-html",
+    elementCodeFilePath: "./src/html/grid-3.html",
+    elementCodeLocation:
+      ".grid-3 .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".grid-3 .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass:
+      ".grid-3 .import-html .code-tabs--content",
+    elementCodeClassPath:
+      ".grid-3 .import-html .code-tabs--container .code",
+    elementCodeInnerText: `<iframe
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522jersey-container%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522jersey-container-img%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cimg%2520src%253D%2522..%252Fassets%252Finter-jersey.png%2522%2520%252F%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%253Csection%2520class%253D%2522jersey-content%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ch1%2520class%253D%2522jersey-title%2522%253EBarcelona%2520Home%2520Kit%253C%252Fh1%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522jersey-category%2522%253EJersey%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520class%253D%2522jersey-price-tag%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%253ERs.%25203999%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cstrike%253ERs.%25207999%253C%252Fstrike%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522discount%2522%253E%2850%2525%29%253C%252Fp%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253C%252Fspan%253E%250A%2520%2520%2520%2520%253C%252Fsection%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522jersey-btn%2520jersey-cart%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cspan%2520class%253D%2522cart-icon%2522%253E%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Cp%2520class%253D%2522cart-text%2522%253EAdd%2520to%2520Cart%253C%252Fp%253E%250A%2520%2520%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 782px; height: 521px; border:0; transform: scale(1); overflow:hidden;"
+    sandbox="allow-scripts allow-same-origin">
+  </iframe>`,
+    elementJSCodeClassPath:
+      ".grid-3 .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: "NA",
+  },
+];
+
+export { elements };
