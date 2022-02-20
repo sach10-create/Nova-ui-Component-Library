edc357e80d22f23cf49755eacabe90e03f4dd63d
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-20 19:13:40 +0530

    feat : added typography

---
+++ components/typography/src/js/properties.js
@@ -0,0 +1,52 @@
+const elements = [
+  {
+    name: "Heading Typography",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .heading-typography .import-html",
+    elementCodeFilePath: "./src/html/heading-typography.html",
+    elementCodeLocation:
+      ".heading-typography .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".heading-typography .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass: ".heading-typography .import-html .code-tabs--content",
+    elementCodeClassPath: ".heading-typography .import-html .code-tabs--container .code",
+    elementCodeInnerText: ``,
+    elementJSCodeClassPath:
+      ".heading-typography .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: `<`,
+  },
+  {
+    name: "Text Position",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .text-position .import-html",
+    elementCodeFilePath: "./src/html/text-position.html",
+    elementCodeLocation:
+      ".text-position .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".text-position .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass: ".text-position .import-html .code-tabs--content",
+    elementCodeClassPath: ".text-position .import-html .code-tabs--container .code",
+    elementCodeInnerText: ``,
+    elementJSCodeClassPath:
+      ".text-position .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: `<`,
+  },
+  {
+    name: "Text Types",
+    rootCodeTabFilePath: "../../../../main/src/html/code-tabs-root.html",
+    rootCodeLocation: "main .main--sub-heading .text-types .import-html",
+    elementCodeFilePath: "./src/html/text-types.html",
+    elementCodeLocation:
+      ".text-types .import-html .code-tabs--content-container .preview",
+    handleCodeTabBtnClass:
+      ".text-types .import-html .code-tabs-btn--container .code-tab--btn",
+    handleCodeTabCodeContentClass: ".text-types .import-html .code-tabs--content",
+    elementCodeClassPath: ".text-types .import-html .code-tabs--container .code",
+    elementCodeInnerText: ``,
+    elementJSCodeClassPath:
+      ".text-types .import-html .code-tabs--container .code-js",
+    elementJSCodeInnerText: `<`,
+  },
+];
+
+export { elements };
