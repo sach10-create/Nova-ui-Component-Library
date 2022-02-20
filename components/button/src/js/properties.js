2eef7a6ad67547f34e659b78482f02d32688c087
sach10-create <65531346+sach10-create@users.noreply.github.com>
2022-02-20 14:29:50 +0530

    feat : added html code in html tabs

--- components/button/src/js/properties.js
+++ components/button/src/js/properties.js
@@ -13,8 +13,8 @@ const elements = [
     elementCodeClassPath:
       ".multiple-button .import-html .code-tabs--container .code",
     elementCodeInnerText: `<iframe
-    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522button-container%2522%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520primary-card-btn%2522%253E%250A%2520%2520%2520%2520%253Cspan%2520class%253D%2522primary-cart-icon%2522%253E%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%253Cp%2520class%253D%2522primary-cart-text%2522%253EAdd%2520to%2520Cart%253C%252Fp%253E%250A%2520%2520%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522primary-cart-icon%2522%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
-    style="width: 816px; height: 428px; border:0; transform: scale(1); overflow:hidden;"
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522button-container%2520d-flex%2520flex-wrap%2520justify-content-space-around%2522%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2520primary-card-btn%2520d-flex%2520align-center%2520justify-content-center%2522%253E%250A%2520%2520%2520%2520%253Cspan%2520class%253D%2522primary-cart-icon%2522%253E%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%253C%252Fspan%253E%250A%2520%2520%2520%2520%253Cp%2520class%253D%2522primary-cart-text%2522%253EAdd%2520to%2520Cart%253C%252Fp%253E%250A%2520%2520%253C%252Fbutton%253E%250A%2520%2520%253Cbutton%2520class%253D%2522btn%2522%253E%250A%2520%2520%2520%2520%253Cdiv%2520class%253D%2522primary-cart-icon%2522%253E%250A%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-shopping-cart%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fdiv%253E%250A%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 842px; height: 428px; border:0; transform: scale(1); overflow:hidden;"
     sandbox="allow-scripts allow-same-origin">
   </iframe>`,
     elementJSCodeClassPath:
@@ -58,8 +58,8 @@ const elements = [
     elementCodeClassPath:
       ".floating-button .import-html .code-tabs--container .code",
     elementCodeInnerText: `<iframe
-    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522floating-button-container%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522floating-button%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-plus%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
-    style="width: 478px; height: 279px; border:0; transform: scale(1); overflow:hidden;"
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522floating-button-container%2520d-flex%2520flex-wrap%2520justify-content-space-around%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522floating-button%2522%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%253Ci%2520class%253D%2522fas%2520fa-plus%2522%253E%253C%252Fi%253E%250A%2520%2520%2520%2520%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 866px; height: 279px; border:0; transform: scale(1); overflow:hidden;"
     sandbox="allow-scripts allow-same-origin">
   </iframe>`,
     elementJSCodeClassPath:
@@ -80,8 +80,8 @@ const elements = [
     elementCodeClassPath:
       ".disabled-button .import-html .code-tabs--container .code",
     elementCodeInnerText: `<iframe
-    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522disabled-button-container%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
-    style="width: 757px; height: 261px; border:0; transform: scale(1); overflow:hidden;"
+    src="https://carbon.now.sh/embed?bg=rgba%28171%2C+184%2C+195%2C+1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%253D%2522disabled-button-container%2520d-flex%2520flex-wrap%2520justify-content-space-around%2522%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-primary%2522%253EPrimary%253C%252Fbutton%253E%250A%2520%2520%2520%2520%253Cbutton%2520class%253D%2522disabled-btn%2520disabled-btn-secondary%2522%253EOutline%253C%252Fbutton%253E%250A%253C%252Fdiv%253E"
+    style="width: 866px; height: 261px; border:0; transform: scale(1); overflow:hidden;"
     sandbox="allow-scripts allow-same-origin">
   </iframe>`,
     elementJSCodeClassPath:
